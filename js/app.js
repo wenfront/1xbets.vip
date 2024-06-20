bcd1e2433a6196ca0641e63166956223779343bf
webfront <webfront65@gmail.com>
2024-06-20 16:33:15 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+let bars = document.querySelector(".bars");
+let showList = document.querySelector(".show-list");
+
+bars.addEventListener("click", function(){
+    showList.classList.toggle("active");
+});
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
